3accd4a95966e14306f82e8ecd09a9e301608538
michaelwang07 <michaelwang07@yahoo.com>
2023-05-06 23:50:13 -0700

    first steps

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,33 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { Flex, Box, Text, Button, Divider } from '@chakra-ui/react';
 
+const Banner = ({purpose, title1, title2, desc1, linkName, buttonText, imageURL}) => (
+  <Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10">
+    <Image src={imageURL} width={500} height={300} alt="banner" />
+    <Box p="5">
+        <Text color="gray.500" fontSize="sm" fontWeight="medium">
+          {purpose}
+        </Text>
+        <Text fontSize="3xl" fontWeight="bold">
+          {title1} <br /> {title2}
+        </Text>
+        <Text fontSize="lg" paddingTop="3" 
+        paddingBotton="3" paddingBottom="3" color="gray.700">
+          {desc1} <br /> {desc2}
+        </Text>
+        <Button fontsize="xl" bg="blue.300" color="white">
+          <Link href={linkName}>{buttonText}</Link>
+        </Button>
+    </Box>
+  </Flex>
+)
+
 export default function Home() {
   return (
-    <div className="">
-
+    <div>
+      <h1>Hello World</h1>
+      <Banner purpose={'For Sale'}/>
+      <Banner purpose={'For Rent'}/>
     </div>
   )
 }
