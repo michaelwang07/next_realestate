0febced816ffc80140c737806a96cc1431e82171
michaelwang07 <michaelwang07@yahoo.com>
2023-05-09 17:46:14 -0700

    images for houses

--- components/Property.jsx
+++ components/Property.jsx
@@ -5,10 +5,15 @@ import {BsGridFill} from 'react-icons/bs';
 import {GoVerified} from 'react-icons/go';
 import millify from 'millify';
 
+import defaultImage from '../assets/house.jpg';
+
 const Property = ({ property: { coverPhoto, price, rentFrequency, rooms, title, baths, area, agency, isVerified, externalID} }) => (
   <Link href={`/property/${externalID}`} passHref>
-    <p>Testing</p>
-    {title}
+    <Flex flexWrap="wrap" w="420px" p="5" paddingTop="0" justifyContent="flex-start" cursor="pointer">
+      <Box>
+        <Image src={coverPhoto ? coverPhoto.url : defaultImage} width={400} height={260} alt='house' />
+      </Box>
+    </Flex>
   </Link>
 )
 
