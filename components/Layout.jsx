5cb840664d11551f2da8d6d2b22f7e100a1646b8
michaelwang07 <michaelwang07@yahoo.com>
2023-05-13 20:40:02 -0700

    navbar

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,12 +1,12 @@
 import Head from 'next/head';
 import {Box} from '@chakra-ui/react';
 
+import Navbar from './Navbar';
+
 const Layout = ({ children }) => {
   return (
     <Box maxWidth="1290px" m="auto">
-        <header>
-          Navbar
-        </header>
+        <Navbar />
         <main>
           {children}
         </main>
