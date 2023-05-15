dc7e7620629b4e102b41ec805874df9eb2e0d9f0
michaelwang07 <michaelwang07@yahoo.com>
2023-05-14 23:57:16 -0700

    Committing something before 12

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   return (
     <Flex p="2" boerderBottom="1px" borderColor="gray.100">
       <Box fontSize="3xl" color="blue.400" fontWeight="bold">
-        <Link href="/" paddingLeft="2">Realtor</Link>
+        <Link href="/" paddingLeft="2">My Realtor App</Link>
       </Box>
       <Spacer />
       <Box>
